f1909aaf7869fba8cc9d4e051cb0cef436a9969d
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-04 22:06:35 +0000

    Letter class added

---
+++ libs/music/letter.js
@@ -0,0 +1,51 @@
+export class Letter {
+	static __c = Symbol();
+	static __d = Symbol();
+	static __e = Symbol();
+	static __f = Symbol();
+	static __g = Symbol();
+	static __a = Symbol();
+	static __b = Symbol();
+
+	static display_name(letter) {
+		switch (letter) {
+			case Letter.__c:
+				return 'C';
+			case Letter.__d:
+				return 'D';
+			case Letter.__e:
+				return 'E';
+			case Letter.__f:
+				return 'F';
+			case Letter.__g:
+				return 'G';
+			case Letter.__a:
+				return 'A';
+			case Letter.__b:
+				return 'B';
+			default:
+				throw new Error(`Illegal 'letter' value provided.`);
+		}
+	}
+
+	static keyboard_friendly_name(letter) {
+		switch (letter) {
+			case Letter.__c:
+				return 'c';
+			case Letter.__d:
+				return 'd';
+			case Letter.__e:
+				return 'e';
+			case Letter.__f:
+				return 'f';
+			case Letter.__g:
+				return 'g';
+			case Letter.__a:
+				return 'a';
+			case Letter.__b:
+				return 'b';
+			default:
+				throw new Error(`Illegal 'letter' value provided.`);
+		}
+	}
+}
