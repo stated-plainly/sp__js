f53146cec35ba47d9fd33d3a8daf0345088d5799
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-05 20:11:24 +0000

    ANSI_Palette class added

--- libs/graphics/ansi.js
+++ libs/graphics/ansi.js
@@ -128,3 +128,17 @@ export class ANSI {
 		return text;
 	}
 }
+
+export class ANSI_Palette {
+	static focal_point = new ANSI().bold().colour(Colour.from_rgb_hex('#ff_c1_45')); // yellow
+
+	static verb = new ANSI().bold().italic().colour(Colour.from_rgb_hex('#ce_17_88')); // pink
+	
+	static noun = new ANSI().bold().colour(Colour.from_rgb_hex('#21_34_e7')); // blue
+
+	static ancillary = new ANSI().italic().dim().colour(Colour.from_rgb_hex('#90_71_71')); // grey
+	
+	static good = new ANSI().bold().colour(Colour.from_rgb_hex('#81_ce_5e')); // green
+	
+	static bad = new ANSI().bold().colour(Colour.from_rgb_hex('#ec_37_37')); // red
+}
