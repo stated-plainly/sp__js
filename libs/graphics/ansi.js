e163c9cf859dfd74c2c37a3fecd0dea91434d2f7
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-05 19:47:56 +0000

    ANSI :: Updated to use enum variant naming convention

--- libs/graphics/ansi.js
+++ libs/graphics/ansi.js
@@ -1,28 +1,28 @@
 import { Colour } from './colour.js';
 
 export class ANSI {
-	static #bold = Symbol();
-	static #dim = Symbol();
-	static #italic = Symbol();
-	static #underline = Symbol();
-	static #blink = Symbol();
-	static #inverse = Symbol();
-	static #hidden = Symbol();
-	static #strikethrough = Symbol();
-	static #colour = Symbol();
-	static #bg_colour = Symbol();
+	static #__bold = Symbol();
+	static #__dim = Symbol();
+	static #__italic = Symbol();
+	static #__underline = Symbol();
+	static #__blink = Symbol();
+	static #__inverse = Symbol();
+	static #__hidden = Symbol();
+	static #__strikethrough = Symbol();
+	static #__colour = Symbol();
+	static #__bg_colour = Symbol();
 
 	static #style_data = {
-		[ANSI.#bold]: {set: 1, reset: 22},
-		[ANSI.#dim]: {set: 2, reset: 22},
-		[ANSI.#italic]: {set: 3, reset: 23},
-		[ANSI.#underline]: {set: 4, reset: 24},
-		[ANSI.#blink]: {set: 5, reset: 25},
-		[ANSI.#inverse]: {set: 7, reset: 27},
-		[ANSI.#hidden]: {set: 8, reset: 28},
-		[ANSI.#strikethrough]: {set: 9, reset: 29},
-		[ANSI.#colour]: {set: 38, reset: 39},
-		[ANSI.#bg_colour]: {set: 48, reset: 39},
+		[ANSI.#__bold]: {set: 1, reset: 22},
+		[ANSI.#__dim]: {set: 2, reset: 22},
+		[ANSI.#__italic]: {set: 3, reset: 23},
+		[ANSI.#__underline]: {set: 4, reset: 24},
+		[ANSI.#__blink]: {set: 5, reset: 25},
+		[ANSI.#__inverse]: {set: 7, reset: 27},
+		[ANSI.#__hidden]: {set: 8, reset: 28},
+		[ANSI.#__strikethrough]: {set: 9, reset: 29},
+		[ANSI.#__colour]: {set: 38, reset: 39},
+		[ANSI.#__bg_colour]: {set: 48, reset: 39},
 	};
 
 	#style_rules;
@@ -32,49 +32,49 @@ export class ANSI {
 	}
 
 	bold() {
-		this.#style_rules.push({rule: ANSI.#bold});
+		this.#style_rules.push({rule: ANSI.#__bold});
 		
 		return this;
 	}
 
 	dim() {
-		this.#style_rules.push({rule: ANSI.#dim});
+		this.#style_rules.push({rule: ANSI.#__dim});
 		
 		return this;
 	}
 
 	italic() {
-		this.#style_rules.push({rule: ANSI.#italic});
+		this.#style_rules.push({rule: ANSI.#__italic});
 		
 		return this;
 	}
 
 	underline() {
-		this.#style_rules.push({rule: ANSI.#underline});
+		this.#style_rules.push({rule: ANSI.#__underline});
 		
 		return this;
 	}
 
 	blink() {
-		this.#style_rules.push({rule: ANSI.#blink});
+		this.#style_rules.push({rule: ANSI.#__blink});
 		
 		return this;
 	}
 
 	inverse() {
-		this.#style_rules.push({rule: ANSI.#inverse});
+		this.#style_rules.push({rule: ANSI.#__inverse});
 		
 		return this;
 	}
 
 	hidden() {
-		this.#style_rules.push({rule: ANSI.#hidden});
+		this.#style_rules.push({rule: ANSI.#__hidden});
 		
 		return this;
 	}
 
 	strikethrough() {
-		this.#style_rules.push({rule: ANSI.#strikethrough});
+		this.#style_rules.push({rule: ANSI.#__strikethrough});
 		
 		return this;
 	}
@@ -84,7 +84,7 @@ export class ANSI {
 			throw new Error(`Illegal 'colour' value provided. Must be of type Colour.`);
 		}
 
-		this.#style_rules.push({rule: ANSI.#bg_colour, colour: colour});
+		this.#style_rules.push({rule: ANSI.#__bg_colour, colour: colour});
 
 		return this;
 	}
@@ -94,7 +94,7 @@ export class ANSI {
 			throw new Error(`Illegal 'colour' value provided. Must be of type Colour.`);
 		}
 
-		this.#style_rules.push({rule: ANSI.#colour, colour: colour});
+		this.#style_rules.push({rule: ANSI.#__colour, colour: colour});
 
 		return this;
 	}
@@ -102,20 +102,20 @@ export class ANSI {
 	apply_to(text) {
 		for (const rule_data of this.#style_rules) {
 			switch (rule_data.rule) {
-				case ANSI.#bold:
-				case ANSI.#dim:
-				case ANSI.#italic:
-				case ANSI.#underline:
-				case ANSI.#blink:
-				case ANSI.#inverse:
-				case ANSI.#hidden:
-				case ANSI.#strikethrough:
+				case ANSI.#__bold:
+				case ANSI.#__dim:
+				case ANSI.#__italic:
+				case ANSI.#__underline:
+				case ANSI.#__blink:
+				case ANSI.#__inverse:
+				case ANSI.#__hidden:
+				case ANSI.#__strikethrough:
 					{
 						const sd = ANSI.#style_data[rule_data.rule];
 						text = `\x1b[${sd.set}m${text}\x1b[${sd.reset}m`;
 					}
 					break;
-				case ANSI.#colour:
+				case ANSI.#__colour:
 					{
 						const sd = ANSI.#style_data[rule_data.rule];
 						const c = rule_data.colour;
