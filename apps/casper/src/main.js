2cebd44d840294aec6eea3544a4830ced407412f
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-11 22:47:12 +0000

    casper :: print_help method implemented, empty build and run methods added

--- apps/casper/src/main.js
+++ apps/casper/src/main.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+import { ANSI_Palette } from '../../../libs/graphics/ansi/ansi_palette.js';
+
+import { build } from './commands/build.js';
+import { run } from './commands/run.js';
 
 switch (process.argv.length) {
     case 3:
@@ -11,8 +15,10 @@ switch (process.argv.length) {
 function process_command(command) {
     switch (command) {
         case 'build':
+			build();
             break;
         case 'run':
+			run();
             break;
         default:
             print_help();
@@ -20,5 +26,15 @@ function process_command(command) {
 }
 
 function print_help() {
-
+	console.log();
+	console.log(ANSI_Palette.ancillary.apply_to('----------------------------------------------'));
+	console.log(` ${ANSI_Palette.focal_point.apply_to('casper')} ${ANSI_Palette.ancillary.apply_to('(compiler)')}`);
+	console.log(ANSI_Palette.ancillary.apply_to('----------------------------------------------'));
+	console.log();
+	console.log(` commands${ANSI_Palette.ancillary.apply_to(':')}`);
+	console.log();
+	console.log(`     ${ANSI_Palette.focal_point.apply_to('build')} ${ANSI_Palette.ancillary.apply_to('::')} ${ANSI_Palette.verb.apply_to('build')} your casper app`);
+	console.log(`       ${ANSI_Palette.focal_point.apply_to('run')} ${ANSI_Palette.ancillary.apply_to('::')} ${ANSI_Palette.verb.apply_to('build')}, then ${ANSI_Palette.verb.apply_to('run')} your casper app`);
+	console.log();
+	console.log(ANSI_Palette.ancillary.apply_to('----------------------------------------------'));
 }
