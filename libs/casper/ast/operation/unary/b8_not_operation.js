baa16ebc01a92e9bf87e5549d25421fa7344bba1
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-12 20:27:55 +0000

    casper (lib) :: B8NotOperation class added

---
+++ libs/casper/ast/operation/unary/b8_not_operation.js
@@ -0,0 +1,19 @@
+import { B8Literal } from '../../literals/b8_literal.js';
+import { CheckType } from '../../../../types/check_type.js';
+import { IllegalArgTypeError } from '../../../../errors/illegal_arg_type_error.js';
+
+export class B8NotOperation {
+	#operand;
+
+	constructor(operand) {
+		if (!CheckType.is_type(operand, B8Literal)) {
+			throw new IllegalArgTypeError('operand', 'B8Literal');
+		}
+
+		this.#operand = operand;
+	}
+
+	get operand() {
+		return this.#operand;
+	}
+}
