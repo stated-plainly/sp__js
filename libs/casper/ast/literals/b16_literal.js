e22ff51aa741c22298f9dff12257dbb29b4742fd
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-12 19:34:33 +0000

    casper (lib) :: B16Literal class added

--- libs/casper/ast/literals/b16_literal.js
+++ libs/casper/ast/literals/b16_literal.js
@@ -1,12 +1,12 @@
 import { IllegalArgTypeError } from '../../../errors/illegal_arg_type_error.js';
 import { CheckCasperValue } from '../../helpers/check_casper_value.js';
 
-export class B8Literal {
+export class B16Literal {
 	#value;
 
 	constructor(value) {
-		if (!CheckCasperValue.is_b8(value)) {
-			throw new IllegalArgTypeError('value', 'b8');
+		if (!CheckCasperValue.is_b16(value)) {
+			throw new IllegalArgTypeError('value', 'b16');
 		}
 
 		this.#value = value;
