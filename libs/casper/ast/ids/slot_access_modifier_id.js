0504e234bfd8cf72fa600932704996d11bbde3c8
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-12 00:36:39 +0000

    casper (lib) :: AccessModifierID renamed to SlotAccessModifierID, TypeAccessModifierID added

---
+++ libs/casper/ast/ids/slot_access_modifier_id.js
@@ -0,0 +1,11 @@
+export class SlotAccessModifierID {
+	#id;
+
+	constructor() {
+		this.#id = Symbol();
+	}
+
+	get id() {
+		return this.#id;
+	}
+}
--- libs/casper/ast/ids/type_access_modifier_id.js
+++ libs/casper/ast/ids/type_access_modifier_id.js
@@ -1,4 +1,4 @@
-export class AccessModifierID {
+export class TypeAccessModifierID {
 	#id;
 
 	constructor() {
