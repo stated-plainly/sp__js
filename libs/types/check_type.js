50f19f36449840ce0c6a496285447db9103b593f
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-12 23:25:11 +0000

    types :: CheckType class updated

--- libs/types/check_type.js
+++ libs/types/check_type.js
@@ -1,35 +1,45 @@
+import { IllegalArgTypeError } from "../errors/illegal_arg_type_error";
+
 export class CheckType {
-	static is_bool(bool) {
-		return typeof bool === 'boolean';
+	static is_bool(value) {
+		return typeof value === 'boolean';
 	}
 
-	static is_string(string) {
-		return typeof string === 'string';
+	static is_null(value) {
+		return value === null;
 	}
 
-	static is_symbol(symbol) {
-		return typeof symbol === 'symbol';
+	static is_undefined(value) {
+		return value === undefined;
 	}
 
-	static is_list(list) {
-		return Array.isArray(list);
+	static is_string(value) {
+		return typeof value === 'string';
+	}
+
+	static is_symbol(value) {
+		return typeof value === 'symbol';
+	}
+
+	static is_list(value) {
+		return Array.isArray(value);
 	}
 
 	static is_type(value, type) {
 		if (!(typeof type === 'function' && 'prototype' in type)) {
-			return false;
+			throw IllegalArgTypeError('type', 'Type');
 		}
 
 		return value instanceof type;
 	}
 
 	static object_has_key(object, key) {
-		if (!(typeof object === 'object')) {
-			return false;
+		if (!(typeof object === 'object' && !CheckType.is_list(object) && !CheckType.is_null(object))) {
+			throw new IllegalArgTypeError('object', 'Object');
 		}
 
 		if (!(typeof key === 'string' || typeof key === 'symbol')) {
-			return false;
+			throw new IllegalArgTypeError('key', 'String | Symbol');
 		}
 
 		return key in object;
