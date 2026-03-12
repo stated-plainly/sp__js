cdcf178aaf47915a92fb8dd2a5d2fe26acb570fd
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-12 19:48:02 +0000

    casper (lib) :: U32Literal class added

---
+++ libs/casper/ast/literals/u32_literal.js
@@ -0,0 +1,18 @@
+import { IllegalArgTypeError } from '../../../errors/illegal_arg_type_error.js';
+import { CheckCasperValue } from '../../helpers/check_casper_value.js';
+
+export class U32Literal {
+	#value;
+
+	constructor(value) {
+		if (!CheckCasperValue.is_u32(value)) {
+			throw new IllegalArgTypeError('value', 'u32');
+		}
+
+		this.#value = value;
+	}
+
+	get value() {
+		return this.#value;
+	}
+}
