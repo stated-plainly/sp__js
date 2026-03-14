4f9e0ead611f6a14cb39b161340bf0f521e2f530
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-14 22:55:25 +0000

    casper (lib) :: PrimitiveID class updated

--- libs/casper/helpers/primitive_id.js
+++ libs/casper/helpers/primitive_id.js
@@ -7,16 +7,38 @@ export class PrimitiveID {
 	static __b32 = Symbol();
 	static __b64 = Symbol();
 	static __b128 = Symbol();
+	static __bsize = Symbol();
 	// h{size}
 	static __h8 = Symbol();
 	static __h16 = Symbol();
 	static __h32 = Symbol();
 	static __h64 = Symbol();
 	static __h128 = Symbol();
+	static __hsize = Symbol();
 	// u{size}
 	static __u8 = Symbol();
 	static __u16 = Symbol();
 	static __u32 = Symbol();
 	static __u64 = Symbol();
 	static __u128 = Symbol();
+	static __usize = Symbol();
+	// i{size}
+	static __i8 = Symbol();
+	static __i16 = Symbol();
+	static __i32 = Symbol();
+	static __i64 = Symbol();
+	static __i128 = Symbol();
+	static __isize = Symbol();
+	// int
+	static __int = Symbol();
+	// range
+	static __range = Symbol();
+	// array
+	static __array = Symbol();
+	// list
+	static __list = Symbol();
+	// char
+	static __char = Symbol();
+	// string
+	static __string = Symbol();
 }
