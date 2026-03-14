637e911d74a3a6f3dc9eada7cb254b0736ce7dc4
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-14 23:13:46 +0000

    casper (lib) :: B16Literal class added

---
+++ libs/casper/ast/literals/b16_literal.js
@@ -0,0 +1,26 @@
+import { CheckType } from '../../../types/check_type.js';
+import { IllegalArgTypeError } from '../../../errors/illegal_arg_type_error.js';
+import { PrimitiveID } from '../../helpers/primitive_id.js';
+
+export class B16Literal {
+	static #type_id = PrimitiveID.__b16;
+	static #value_checker_regex = /^[01]{4}_[01]{4}__[01]{4}_[01]{4}$/;
+
+	#value;
+
+	constructor(value) {
+		if (!(CheckType.is_string(value) && B16Literal.#value_checker_regex.test(value))) {
+			throw new IllegalArgTypeError('value', 'String<b16 :: 0000_0000__0000_0000 to 1111_1111__1111_1111>');
+		}
+
+		this.#value = value;
+	}
+
+	get type_id() {
+		return B16Literal.#type_id;
+	}
+
+	get value() {
+		return this.#value;
+	}
+}
