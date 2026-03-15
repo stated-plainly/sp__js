6ef4e34f60cc4eb4f1e32d99373d6684ec37d6c8
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-15 00:12:03 +0000

    casper (lib) :: B8Literal updated

--- libs/casper/ast/literals/b8_literal.js
+++ libs/casper/ast/literals/b8_literal.js
@@ -1,17 +1,25 @@
+import { CharParser } from '../../../text/char_parser.js';
 import { CheckType } from '../../../types/check_type.js';
 import { IllegalArgTypeError } from '../../../errors/illegal_arg_type_error.js';
 import { PrimitiveID } from '../../helpers/primitive_id.js';
 
 export class B8Literal {
 	static #type_id = PrimitiveID.__b8;
-	static #value_checker_regex = /^[01]{4}_[01]{4}$/;
 
 	#value;
 
 	constructor(value) {
-		if (!(CheckType.is_string(value) && B8Literal.#value_checker_regex.test(value))) {
-			throw new IllegalArgTypeError('value', 'String<b8 :: 0000_0000 to 1111_1111>');
+		if (!(CheckType.is_string(value))) {
+			throw new IllegalArgTypeError('value', 'String');
 		}
+
+		new CharParser()
+			.load(value)
+			.chainable_expect(true)
+			.expect(CharParser.__binary).expect(CharParser.__binary).expect(CharParser.__binary).expect(CharParser.__binary)
+			.expect('_')
+			.expect(CharParser.__binary).expect(CharParser.__binary).expect(CharParser.__binary).expect(CharParser.__binary)
+			.expect(CharParser.__end);
 
 		this.#value = value;
 	}
